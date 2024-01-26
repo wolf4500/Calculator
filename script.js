770f913cc9d49f12d95d187aa64fd2296e57aca0
Aaron Dang <aarondang4500@gmail.com>
2024-01-26 15:11:31 -0600

    Create calculator html outline

--- script.js
+++ script.js
@@ -14,4 +14,15 @@ function divide(a, b) {
     return a / b;
 }
 
+let numberA = 3;
+let operator = "+";
+let numberB = 5;
 
+
+function operate(valueA, symbol, valueB) {
+    let total = 0;
+    if(symbol == "+") {
+       total = add(valueA, valueB);
+    }
+    return total;
+}
