9c6581ff786245f5473f350b4ae8fa2de0806caa
Aaron Dang <aarondang4500@gmail.com>
2024-01-28 20:17:05 -0600

    Allow calculator to populate display with numbers

--- script.js
+++ script.js
@@ -28,9 +28,61 @@ function operate(valueA, symbol, valueB) {
 }
 
 const display = document.querySelector(".display");
-const button = document.querySelector(".digits");
-let currentTotal = 0;
+const button = document.querySelector(".inner-calc");
+let currentDisplay = "";
 
 button.addEventListener("click", (event) => {
-    console.log(event);
+    if(event.target.id == "seven")
+    {
+        currentDisplay = `${currentDisplay}7`;
+        display.innerHTML = currentDisplay;
+    }
+    if(event.target.id == "eight")
+    {
+        currentDisplay = `${currentDisplay}8`;
+        display.innerHTML = currentDisplay;
+    }
+    if(event.target.id == "nine")
+    {
+        currentDisplay = `${currentDisplay}9`;
+        display.innerHTML = currentDisplay;
+    }
+    if(event.target.id == "four")
+    {
+        currentDisplay = `${currentDisplay}4`;
+        display.innerHTML = currentDisplay;
+    }
+    if(event.target.id == "five")
+    {
+        currentDisplay = `${currentDisplay}5`;
+        display.innerHTML = currentDisplay;
+    }
+    if(event.target.id == "six")
+    {
+        currentDisplay = `${currentDisplay}6`;
+        display.innerHTML = currentDisplay;
+    }
+    if(event.target.id == "one")
+    {
+        currentDisplay = `${currentDisplay}1`;
+        display.innerHTML = currentDisplay;
+    }
+    if(event.target.id == "two")
+    {
+        currentDisplay = `${currentDisplay}2`;
+        display.innerHTML = currentDisplay;
+    }
+    if(event.target.id == "three")
+    {
+        currentDisplay = `${currentDisplay}3`;
+        display.innerHTML = currentDisplay;
+    }
+    if(event.target.id == "zero")
+    {
+        currentDisplay = `${currentDisplay}0`;
+        display.innerHTML = currentDisplay;
+    }
+   
 })
+
+
